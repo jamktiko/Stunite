296088cad66549163dbc88966261a7d373ababe4
Inka Männikkö <AD1541@student.jamk.fi>
2024-09-09 13:45:55 +0300

    komponenttien luomista

--- frontend/angular-pwa/src/app/app.routes.ts
+++ frontend/angular-pwa/src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './features/home/home.component';
+import { EventsComponent } from './features/events/events.component';
+import { AssociationsComponent } from './features/associations/associations.component';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+  { path: '', component: HomeComponent }, 
+  { path: 'home', component: HomeComponent },
+  { path: 'events', component: EventsComponent },
+  { path: 'associations', component: AssociationsComponent },
+
+];
