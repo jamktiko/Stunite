e58e2c4106585f07215395e024b7165be1f3f443
Inka Männikkö <AD1541@student.jamk.fi>
2024-09-11 10:15:38 +0300

    tapahtuma sivun tapahtuma kortit ja niihin tietojen hakeminen, tapahtuma sivun hakukenttä

--- frontend/angular-pwa/src/app/features/events/event.service.ts
+++ frontend/angular-pwa/src/app/features/events/event.service.ts
@@ -1,15 +1,16 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs/internal/Observable';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class EventService {
-  private apiUrl = 'assets/fakedata.json';
-
+  private apiUrl = 'assets/fakedata.json'; 
+  
   constructor(private http: HttpClient) {}
-  getEvents(): Observable<any> {
-    return this.http.get<any>(this.apiUrl);
+
+  getEvents(): Observable<any[]> {
+    return this.http.get<any[]>(this.apiUrl);
   }
 }
--- frontend/angular-pwa/src/app/features/events/events.component.ts
+++ frontend/angular-pwa/src/app/features/events/events.component.ts
@@ -2,22 +2,42 @@ import { Component, OnInit } from '@angular/core';
 import { EventService } from './event.service';
 import { EventcardComponent } from './eventcard/eventcard.component';
 import { CommonModule } from '@angular/common';
-
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-events',
   standalone: true,
-  imports: [EventcardComponent, CommonModule],
+  imports: [EventcardComponent, CommonModule, FormsModule],
   templateUrl: './events.component.html',
-  styleUrl: './events.component.css',
+  styleUrls: ['./events.component.css'],
 })
 export class EventsComponent implements OnInit {
-  eventData: any;
+  eventData: any[] = [];
+
+  filteredEventData: any[] = [];
+  searchTerm: string = '';
 
   constructor(private eventService: EventService) {}
+
   ngOnInit(): void {
     this.eventService.getEvents().subscribe((data) => {
       this.eventData = data;
+      this.filteredEventData = data;
+      console.log('Tapahtumat ladattu:', this.eventData);
     });
+  }
+
+  onSearch() {
+    console.log('Hakutermi:', this.searchTerm);
+
+    if (this.searchTerm) {
+      this.filteredEventData = this.eventData.filter((event) =>
+        event.eventName.toLowerCase().includes(this.searchTerm.toLowerCase())
+      );
+      console.log('Suodatetut tapahtumat:', this.filteredEventData);
+    } else {
+      this.filteredEventData = this.eventData;
+      console.log('Näytetään kaikki tapahtumat');
+    }
   }
 }
