28e68179049754416232522b9cae9eadbf87c306
Inka Männikkö <AD1541@student.jamk.fi>
2024-09-17 14:11:29 +0300

    Etusivun tapahtumakuvakkeet näkyy, mutta vaatii muokkausta

--- frontend/angular-pwa/src/app/features/events/eventcard/eventcard.component.ts
+++ frontend/angular-pwa/src/app/features/events/eventcard/eventcard.component.ts
@@ -1,10 +1,11 @@
 import { Component, Input } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-eventcard',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, CommonModule],
   templateUrl: './eventcard.component.html',
   styleUrl: './eventcard.component.css',
 })
