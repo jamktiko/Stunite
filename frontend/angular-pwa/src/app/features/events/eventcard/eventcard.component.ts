cb18cca5406577086991481c37689b3125db8f4e
Inka Männikkö <AD1541@student.jamk.fi>
2024-09-17 13:20:25 +0300

    tapahtumien nökyminen etusivulla aloitettu

--- frontend/angular-pwa/src/app/features/events/eventcard/eventcard.component.ts
+++ frontend/angular-pwa/src/app/features/events/eventcard/eventcard.component.ts
@@ -10,4 +10,5 @@ import { RouterLink } from '@angular/router';
 })
 export class EventcardComponent {
   @Input() event: any;
+  @Input() onlyImage: boolean = false;
 }
--- frontend/angular-pwa/src/app/features/home/home.component.ts
+++ frontend/angular-pwa/src/app/features/home/home.component.ts
@@ -1,11 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CalendarComponent } from '../../shared/calendar/calendar.component';
+import { EventService } from '../events/event.service';
+import { CommonModule } from '@angular/common';
+import { EventcardComponent } from '../events/eventcard/eventcard.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CalendarComponent],
+  imports: [CalendarComponent, CommonModule, EventcardComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
 })
-export class HomeComponent {}
+export class HomeComponent {
+  featuredEvents: any[] = [];
+  constructor(private eventService: EventService) {}
+  ngOnInit(): void {
+    this.eventService.getEvents().subscribe((events) => {
+      this.featuredEvents = events.slice(0, 4);
+    });
+  }
+}
