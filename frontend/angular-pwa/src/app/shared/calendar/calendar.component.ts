84cc1e7d9c698d663e70867f8640462b99357390
Inka Männikkö <AD1541@student.jamk.fi>
2024-09-18 12:11:43 +0300

    etusivu muutoksia ja paketin version vaihtaminen

--- frontend/angular-pwa/src/app/shared/calendar/calendar.component.ts
+++ frontend/angular-pwa/src/app/shared/calendar/calendar.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { IgxCalendarModule } from 'igniteui-angular';
+import { EventService } from '../../features/events/event.service';
 
 @Component({
   selector: 'app-calendar',
@@ -8,4 +9,23 @@ import { IgxCalendarModule } from 'igniteui-angular';
   templateUrl: './calendar.component.html',
   styleUrls: ['./calendar.component.css'],
 })
-export class CalendarComponent {}
+export class CalendarComponent implements OnInit {
+  eventDates: Date[] = []; // Array that has event dates
+  constructor(private eventService: EventService) {}
+  ngOnInit(): void {
+    this.eventService.getEvents().subscribe((events) => {
+      this.eventDates = events.map(
+        (event) => new Date(event.date.split('.').reverse().join('-'))
+      );
+    });
+  }
+
+  isEventDay(day: Date): boolean {
+    return this.eventDates.some(
+      (eventDate) =>
+        eventDate.getDate() === day.getDate() &&
+        eventDate.getMonth() === day.getMonth() &&
+        eventDate.getFullYear() === day.getFullYear()
+    );
+  }
+}
