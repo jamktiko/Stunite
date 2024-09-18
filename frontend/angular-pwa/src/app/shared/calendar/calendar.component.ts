fa7cb9166b1c52144e9b11b9d0d48e57a556a199
Inka Männikkö <AD1541@student.jamk.fi>
2024-09-18 12:39:16 +0300

    kalentarissa näkyy päivät jolloin on tapahtumia

--- frontend/angular-pwa/src/app/shared/calendar/calendar.component.ts
+++ frontend/angular-pwa/src/app/shared/calendar/calendar.component.ts
@@ -1,31 +1,39 @@
-import { Component, OnInit } from '@angular/core';
-import { IgxCalendarModule } from 'igniteui-angular';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import {
+  IgxCalendarModule,
+  IgxCalendarComponent,
+  DateRangeDescriptor,
+  DateRangeType,
+} from 'igniteui-angular';
 import { EventService } from '../../features/events/event.service';
 
 @Component({
   selector: 'app-calendar',
   standalone: true,
-  imports: [IgxCalendarModule],
+  imports: [IgxCalendarModule, IgxCalendarComponent],
   templateUrl: './calendar.component.html',
   styleUrls: ['./calendar.component.css'],
 })
 export class CalendarComponent implements OnInit {
+  @ViewChild('MyCalendar', { static: true })
+  public calendar!: IgxCalendarComponent;
+
   eventDates: Date[] = []; // Array that has event dates
+
   constructor(private eventService: EventService) {}
+
   ngOnInit(): void {
     this.eventService.getEvents().subscribe((events) => {
       this.eventDates = events.map(
-        (event) => new Date(event.date.split('.').reverse().join('-'))
+        (event) => new Date(event.date.split('.').reverse().join('-')) // Format date
       );
-    });
-  }
 
-  isEventDay(day: Date): boolean {
-    return this.eventDates.some(
-      (eventDate) =>
-        eventDate.getDate() === day.getDate() &&
-        eventDate.getMonth() === day.getMonth() &&
-        eventDate.getFullYear() === day.getFullYear()
-    );
+      this.calendar.specialDates = this.eventDates.map((date) => {
+        return {
+          type: DateRangeType.Specific,
+          dateRange: [date],
+        } as DateRangeDescriptor;
+      });
+    });
   }
 }
