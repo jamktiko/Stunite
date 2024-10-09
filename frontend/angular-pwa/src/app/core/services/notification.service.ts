4eed5da4f751c123ad921fd94877acfb3f864534
Inka Männikkö <AD1541@student.jamk.fi>
2024-10-09 13:38:05 +0300

    npm paketti muutoksia ja toastr käyttöön ottoa

---
+++ frontend/angular-pwa/src/app/core/services/notification.service.ts
@@ -0,0 +1,41 @@
+// import { Injectable } from '@angular/core';
+// import { ToastrService } from 'ngx-toastr';
+
+// @Injectable({
+//   providedIn: 'root',
+// })
+// export class NotificationService {
+//   constructor(private toastr: ToastrService) {}
+
+//   // success notifications
+//   success(message: string, title: string = 'Success') {
+//     this.toastr.success(message, title, {
+//       timeOut: 3000,
+//       positionClass: 'toast-top-right',
+//     });
+//   }
+
+//   // error notifications
+//   error(message: string, title: string = 'Error') {
+//     this.toastr.error(message, title, {
+//       timeOut: 3000,
+//       positionClass: 'toast-top-right',
+//     });
+//   }
+
+//   // info notifications
+//   info(message: string, title: string = 'Info') {
+//     this.toastr.info(message, title, {
+//       timeOut: 3000,
+//       positionClass: 'toast-top-right',
+//     });
+//   }
+
+//   // warning notifications
+//   warning(message: string, title: string = 'Warning') {
+//     this.toastr.warning(message, title, {
+//       timeOut: 3000,
+//       positionClass: 'toast-top-right',
+//     });
+//   }
+// }
