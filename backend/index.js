6732c829e470f9c8be5f959beac750267fb27ca8
eetuaro <ad1319@student.jamk.fi>
2024-10-30 12:50:29 +0200

    Korjailua aws varten

--- backend/index.js
+++ backend/index.js
@@ -19,10 +19,14 @@ const app = express();
 app.use(cookieParser());
 
 app.use(express.json());
-app.use(express.static(__dirname + '/dist/angular-pwa'));
+app.use(express.static(__dirname + '/dist/angular-pwa/browser'));
+
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL || 'http://localhost:4200',
+    origin: [
+      'http://localhost:4200',
+      'http://stunite.eu-north-1.elasticbeanstalk.com/',
+    ],
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     credentials: true,
   })
